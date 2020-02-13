419338a4cc773fda458bef9e72bd4b2f1fd29604
drogi17 <38162697+drogi17@users.noreply.github.com>
2020-02-13 17:30:41 +0200

    Add files via upload

--- modules/user.py
+++ modules/user.py
@@ -1,11 +1,11 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+# from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
 
-def set_password(password):
-    return generate_password_hash(password)
+# def set_password(password):
+#     return generate_password_hash(password)
 
-def check_password(password_hash, password):
-    return check_password_hash(password_hash, password)
+# def check_password(password_hash, password):
+#     return check_password_hash(password_hash, password)
 
 def avatar(size, email):
     digest = md5(email.lower().encode('utf-8')).hexdigest()
