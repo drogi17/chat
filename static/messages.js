ccc70e65c0a5a35ce691a8f5cee5d7473ef01168
drogi17 <38162697+drogi17@users.noreply.github.com>
2020-02-14 14:00:16 +0200

    Add files via upload

---
+++ static/messages.js
@@ -0,0 +1,60 @@
+var check_mess = 1;
+var all_text = 0;
+
+function ord( string ) {
+    var bytes = [];
+    for(var i = 0; i < string.length; i++) {
+        var char = string.charCodeAt(i);
+        bytes.push(char);
+    }
+    return bytes;
+}
+
+
+
+$(document).ready(function() {
+
+    var socket = io.connect('/');
+
+    socket.on('connect', function() {
+        socket.send({'data': ord('')});
+    });
+
+    socket.on('message', function(text) {
+        console.log(text[0] == 'exit' && text[1] == document.cookie);
+        if (text[0] == 'exit' && text[1] == document.cookie){
+            document.cookie = "username="
+            window.location.href = '/';
+        }
+        html_to_add = '';
+        var max = text.length;
+        var i = 0;
+
+        while (i <= max-1) {
+            // console.log(text[i]);
+            var img = "https://www.iphones.ru/wp-content/uploads/2017/02/AA-4.png";
+            var html_element =  "<div class='message-block'><img src='" + text[i][1] + 
+                                "' class='message-avatar'><div class='message-data'><span class='message-user'>" + text[i][0] +
+                                "</span><br><span class='message-text'>" + text[i][2] + "</span></div></div>";
+            html_to_add += html_element;
+            i++;
+        }
+        document.getElementById("text").innerHTML = html_to_add;
+        var block = document.getElementById("chat-messages");
+        block.scrollTop = 10000000 ;
+        all_text = text.length;
+        document.getElementById("message_text").value = '';
+    });
+    $('#send-button').on('click', function() {
+        var send_data = document.getElementById("message_text").value;
+        socket.send({'data':ord(send_data)});
+    });
+    document.addEventListener('keydown', function(event) {
+      if (event.code == 'Enter') {
+        var send_data = document.getElementById("message_text").value;
+        socket.send({'data':ord(send_data)});
+      }
+    });
+
+});
+
