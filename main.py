21904cf372a2abe621d58433cf857d031be4ffd5
drogi17 <38162697+drogi17@users.noreply.github.com>
2020-02-14 14:29:01 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, jsonify, send_from_directory, redirect, make_response
+from flask_socketio import SocketIO, send #библиотека
 from modules.sqlite_inquiries import DataBase
 import os, sys
 import json
@@ -15,8 +16,20 @@ def get_link_in_str(string):
                 string = string.replace(all_words, '<a href="' + all_words + '" target="_blank">' + all_words.replace('http://', '') + '</a>')
     return string
 
+def bytes_to_str(byte):
+    string = ""
+    if byte:
+        for or_d in byte:
+            string += chr(or_d)
+        # print(string)
+        return string
+    else:
+        return ''
+
 db = DataBase('data/data.db')
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
 command_list = {
     '?clear': db.delete,
 }
@@ -43,9 +56,11 @@ def login():
     return resp
 
 
-@app.route('/send_text', methods=['POST'])
-def send_text():
-    data = str(request.data.decode('utf-8')).replace("'", '"').replace('<', '&lt;').replace('>', '&gt;')
+@socketio.on('message')
+def handleMessage(msg):
+    # print(msg)
+    msg = bytes_to_str(msg['data'])
+    data = str(msg).replace("'", '"').replace('<', '&lt;').replace('>', '&gt;')
     if len(data) > 1000:
         data = data[:1000]
     data = get_link_in_str(data)
@@ -53,35 +68,25 @@ def send_text():
         if emoj in data:
             data = data.replace(emoj, '<img src="' + amoji_dict[emoj] + '" title="' + emoj + '" class="emoji-img">')
     adr = str(request.cookies.get('username'))
-    if adr == '':
-        return {'exit': 1}
     where = ['name = "' + str(adr) + '"']
     try:
         id_usr = db.simple_select('users', 'id', where)[0][0]
-        if data != "":
+        if data != "" and id_usr:
             data_inc = {'user_id': id_usr, 'message': data}
             db.insert('messages', data_inc)
         if data in command_list:
             command = command_list.get(data)
             command('messages', '')
         elif data == '?exit' or not id_usr:
-            resp = make_response({'exit': 1})
-            resp.set_cookie('username', '')
             data_inc = {'user_id': 0, 'message': '[' + str(adr) + '] LEFT'}
             db.insert('messages', data_inc)
-            return resp
+            send(['exit', 'username='+adr], broadcast=True)
+        data_to_send = db.custom_request("""    SELECT users.name, users.avatar, messages.message
+                                                FROM messages 
+                                                INNER JOIN users ON messages.user_id = users.id;""")
+        send(data_to_send, broadcast=True)
     except IndexError:
-        resp = make_response({'exit': 1})
-        resp.set_cookie('username', '')
-        return resp
-    return {'exit': 0}
-
-@app.route('/get_messages', methods=['GET'])
-def get_messages():
-    data_to_send = db.custom_request("""   SELECT users.name, users.avatar, messages.message
-                            FROM messages 
-                            INNER JOIN users ON messages.user_id = users.id;""")
-    return {'text':data_to_send}
+        send(['exit', 'username='+adr], broadcast=True)
 
 port = 5000
 host = '127.0.0.1'
@@ -98,5 +103,6 @@ if '--port' in sys.argv:
         port = int(sys.argv[nomb])
     except:
         pass
+
 if __name__ == "__main__":
 	app.run(debug=True, host=host, port=port)
